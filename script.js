6708d8fadc76d2f2a3a4a831373b708b153f1387
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-02 08:23:13 -0600

    Updated HTML and created basic functions for the operations

---
+++ script.js
@@ -0,0 +1,25 @@
+// Functions to compute the basic 4 operations
+// Add
+function add(a, b) {
+    return a + b;
+}
+
+// Subtract
+function subtract(a, b) {
+    return a - b;
+}
+
+// Multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+// Divide
+function divide(a, b) {
+    if (b === 0) {
+        return "Cannot divide by zero";
+    }
+    return a / b;
+}
+
+
